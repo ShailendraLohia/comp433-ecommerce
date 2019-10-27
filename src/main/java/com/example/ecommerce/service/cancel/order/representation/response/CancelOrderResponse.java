7dd4b82b280ed5bcaf76cfb8c3c4039c7e5150d7
ANTHONY-XPS13\Anthony <alifonti@luc.edu>
2019-10-27 14:18:29 -0500

    added CancelOrder representation

--- src/main/java/com/example/ecommerce/service/cancel/order/representation/response/CancelOrderResponse.java
+++ src/main/java/com/example/ecommerce/service/cancel/order/representation/response/CancelOrderResponse.java
@@ -1,4 +1,18 @@
 package com.example.ecommerce.service.cancel.order.representation.response;
 
+import com.example.ecommerce.model.cancel.order.ReturnItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Set;
+
+@XmlRootElement
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CancelOrderResponse {
+    private String cancelOrderId;
+    private Set<ReturnItems> returnItems;
 }
