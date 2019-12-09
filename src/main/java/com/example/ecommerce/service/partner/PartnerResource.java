bd26eaccecbd894b5ba222f33f4a425cbf33a70d
ANTHONY-XPS13\Anthony <alifonti@luc.edu>
2019-12-09 13:35:47 -0600

    added preflight methods to Resource

--- src/main/java/com/example/ecommerce/service/partner/PartnerResource.java
+++ src/main/java/com/example/ecommerce/service/partner/PartnerResource.java
@@ -2,18 +2,54 @@ package com.example.ecommerce.service.partner;
 
 import com.example.ecommerce.service.partner.representation.PartnerDetails;
 import com.example.ecommerce.service.partner.workflow.PartnerActivity;
+import org.apache.cxf.rs.security.cors.CorsHeaderConstants;
+import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
+import org.apache.cxf.rs.security.cors.LocalPreflight;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 @Controller("partnerService")
+@CrossOriginResourceSharing(
+        allowOrigins = {"http://localhost:3000"},
+        allowCredentials = true,
+        allowHeaders = {
+                "'Accept': 'application/json'",
+                "'Content-Type': 'application/json'"
+        }
+)
 @Path("/partners")
 public class PartnerResource {
+    @Context
+    private HttpHeaders headers;
+
+    @OPTIONS
+    @Path("/")
+    @LocalPreflight
+    public Response options(){
+        String origin = headers.getOrigin();
+        if("http://localhost:3000".equals(origin)) {
+            return Response.ok()
+                    .header(CorsHeaderConstants.HEADER_AC_ALLOW_METHODS, "POST")
+                    .header(CorsHeaderConstants.HEADER_AC_ALLOW_CREDENTIALS, "true")
+                    .header(CorsHeaderConstants.HEADER_AC_ALLOW_ORIGIN, "http://localhost:3000")
+                    .header(CorsHeaderConstants.HEADER_AC_ALLOW_HEADERS, "Content-Type")
+                    .build();
+        }
+        else {
+            return Response.ok().build();
+        }
+    }
 
     @Autowired
     private PartnerActivity partnerService;
@@ -23,6 +59,4 @@ public class PartnerResource {
         return new ResponseEntity<>(partnerService.addPartnerData(partnerDetails.getPartnerDetails()),
                 HttpStatus.CREATED);
     }
-
-
 }
